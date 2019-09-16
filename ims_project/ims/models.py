65c110cd7a067eeaa58270cf7daf9c169077f47a
KYD <kydkang@gmail.com>
2019-09-16 07:37:35 -0500

    20190916001

--- ims_project/ims/models.py
+++ ims_project/ims/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Index(models.Model):
+    name = models.CharField(max_length=100,  help_text='Enter field documentation', unique=True)
+
+    def __str__(self):
+        return self.name
