7350f1f9432d53ac3d1f8cf51578274a280e1d72
KYD <kydkang@gmail.com>
2019-09-17 16:39:06 -0500

    20190917-004

--- ims_project/ims/urls.py
+++ ims_project/ims/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-
+    path('dept/<int:id>/', views.IndexListView.as_view(), name='index_list'),
+    path('dept/<int:id1>/index/<int:pk>', views.IndexDetailView.as_view(), name='index_detail')
 ]
--- ims_project/ims/views.py
+++ ims_project/ims/views.py
@@ -1,14 +1,26 @@
 from django.views.generic import ListView
-from .models import Department
+from .models import Department, Index
 
 class HomePageView(ListView):
     model = Department                   ###  Or, queryset = Post.objects.all()
     template_name = 'ims/home.html'
     ###  [[ context_object_name = ‘posts’ ]]  instead of the defalut ‘object_list’
 
+class IndexListView(ListView): 
+    # model = Index
+    template_name = 'ims/index_list.html' 
+    def get_queryset(self):
+        return Index.objects.filter(department=self.kwargs['id'])
 
-# class AboutPageView(TemplateView):
-#     template_name = 'ims/about.html'
+class IndexDetailView(ListView):
+    model = Index 
+    template_name = 'ims/index_detail.html'
+
+    # somehow, the following function is need to pass the object to template....
+    def get_context_data(self, **kwargs):
+        context = super(IndexDetailView, self).get_context_data(**kwargs)
+        context['object'] = Index.objects.get(id=self.kwargs['pk'])
+        return context
 
 
 
