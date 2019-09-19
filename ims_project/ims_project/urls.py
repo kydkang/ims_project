aa605f6a43d23636319e1c4f96c8e8338805fa36
KYD <kydkang@gmail.com>
2019-09-19 17:04:27 -0500

    20190919 last

--- ims_project/ims_project/urls.py
+++ ims_project/ims_project/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('', include('ims.urls')),
 
-    path('', TemplateView.as_view(template_name='home.html'), name='home'), 
-    path('index/', include('ims.urls'), name='index_home'), 
+    # path('', TemplateView.as_view(template_name='home.html'), name='home'), 
+    path('', include('ims.urls')), 
     path('admin/',   admin.site.urls), 
     path('users/', include('users.urls')), 
     path('users/', include('django.contrib.auth.urls')), 
