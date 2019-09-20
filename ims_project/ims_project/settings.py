018f057fddc7e307efb18d42bcd15f0106a67035
KYD <kydkang@gmail.com>
2019-09-20 06:38:29 -0500

    20190920-AfterChapter9-homeMerged

--- ims_project/ims_project/settings.py
+++ ims_project/ims_project/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 AUTH_USER_MODEL = 'users.CustomUser' 
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'index_home'
+LOGOUT_REDIRECT_URL = 'index_home'
 
 
--- ims_project/ims_project/urls.py
+++ ims_project/ims_project/urls.py
@@ -3,10 +3,6 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('', include('ims.urls')),
-
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'), 
     path('', include('ims.urls')), 
     path('admin/',   admin.site.urls), 
     path('users/', include('users.urls')), 
